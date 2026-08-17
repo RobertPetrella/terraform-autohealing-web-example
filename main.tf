2e9a18aa9f6c83553f97e33d7aa9ed6eee06a619
Robert Petrella <36828415+RobertPetrella@users.noreply.github.com>
2026-08-17 21:51:43 +1000

    create load balancer + health probe

--- main.tf
+++ main.tf
@@ -128,3 +128,29 @@ resource "azurerm_virtual_machine_extension" "deploy_nginx" {
   }
   SETTINGS
 }
+
+#Create public load balancers and health probe to ensure VMs stay up
+resource "azurerm_lb" "web_load_balancer" {
+  name = "azure_web_load_balancer"
+  location = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku = "Standard"
+
+  frontend_ip_configuration {
+    name = "lb_frontend_ip"
+    public_ip_address_id = azurerm_public_ip.public_ip.id
+  }
+}
+
+#Backend address pool for load balancer
+resource "azurerm_lb_backend_address_pool" "backend_pool" {
+  loadbalancer_id = azurerm_lb.web_load_balancer.id
+  name = "lb-pool"
+}
+
+resource "azurerm_lb_probe" "health_probe" {
+  loadbalancer_id = azurerm_lb.web_load_balancer.id
+  name = "lb-probe"
+  port = 80
+  
+}
