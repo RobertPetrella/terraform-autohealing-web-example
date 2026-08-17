72e7eec3d60e3325a5363edceb2a41d188b2013f
Robert Petrella <36828415+RobertPetrella@users.noreply.github.com>
2026-08-17 23:27:43 +1000

    fix network_interfacr_ids

--- main.tf
+++ main.tf
@@ -93,7 +93,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   name = "azurevm-vm"
   location = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  network_interface_ids = [azurerm_network_interface.nic.id[count.index]]
+  network_interface_ids = [azurerm_network_interface.nic[count.index].id]
   size = "Standard_B1s"
 
   admin_ssh_key {
