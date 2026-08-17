3952632c6f2b06837864e9915a4ae56baef0b087
Robert Petrella <36828415+RobertPetrella@users.noreply.github.com>
2026-08-17 23:11:42 +1000

    added user+pass lines + fixes

--- main.tf
+++ main.tf
@@ -89,10 +89,11 @@ resource "azurerm_network_interface" "nic" {
 
 #Configure Azure Ubuntu VMs
 resource "azurerm_linux_virtual_machine" "vm" {
+  count = 2
   name = "azurevm-vm"
   location = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  network_interface_ids = [azurerm_network_interface.nic.id,]
+  network_interface_ids = [azurerm_network_interface.nic.id[count.index]]
   size = "Standard_B1s"
 
   admin_ssh_key {
@@ -111,6 +112,9 @@ resource "azurerm_linux_virtual_machine" "vm" {
     sku = "22_04-lts"
     version = "latest"
   }
+  admin_username = var.vm_admin_username
+  admin_password = var.vm_admin_password
+  disable_password_authentication = false
   provision_vm_agent = true
   allow_extension_operations = true #Allows extensions to run in VM
 }
