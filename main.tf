7949e0ead661292d97dec18aa19c3736968564b5
Robert Petrella <36828415+RobertPetrella@users.noreply.github.com>
2026-08-18 08:35:32 +1000

    created + moved providers to new file

--- main.tf
+++ main.tf
@@ -1,20 +1,3 @@
-#Configure Azure provider for Terraform
-terraform {
-    required_providers {
-      azurerm = {
-        source = "hashicorp/azurerm"
-        version = "~> 3.0.2"
-      }
-    }
-
-    required_version = ">=1.15.0"
-}
-
-provider "azurerm" {
-    features {}
-  
-}
-
 resource "azurerm_resource_group" "rg" {
     name = "azure-resource"
     location = "eastus2"
---
+++ providers.tf
@@ -0,0 +1,16 @@
+#Configure Azure provider for Terraform
+terraform {
+    required_providers {
+      azurerm = {
+        source = "hashicorp/azurerm"
+        version = "~> 3.0.2"
+      }
+    }
+
+    required_version = ">=1.15.0"
+}
+
+provider "azurerm" {
+    features {}
+  
+}
