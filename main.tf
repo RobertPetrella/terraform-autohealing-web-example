8e1c70d823e1a10eef3e914ee7ee1b10bb8ea6a1
Robert Petrella <36828415+RobertPetrella@users.noreply.github.com>
2026-08-17 11:47:59 +1000

    added subnet;nic + fixed name

--- main.tf
+++ main.tf
@@ -21,8 +21,29 @@ resource "azurerm_resource_group" "rg" {
 }
 
 #setup Virtual Networks for Azure load balanced VMs
-resource "azurevm_virtual_network" "vnet" {
+resource "azurerm_virtual_network" "vnet" {
   name = "azurevm-vnet"
   address_space = ["10.0.0.0/16"]
   location = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name = "azurevm-subnet"
+  resource_group_name = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes =  ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  count = 2
+  name = "azurevm-nic-${count.index}"
+  location = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name = "internal"
+    subnet_id = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  
 }
