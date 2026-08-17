cf46792308675d532d688ed2d8c001b85cf1a3e4
Robert Petrella <36828415+RobertPetrella@users.noreply.github.com>
2026-08-17 22:04:28 +1000

    updated vm size

--- main.tf
+++ main.tf
@@ -93,7 +93,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   location = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.nic.id,]
-  size = "Standard_DS1_v2"
+  size = "Standard_B1s"
 
   admin_ssh_key {
     username = "admin"
