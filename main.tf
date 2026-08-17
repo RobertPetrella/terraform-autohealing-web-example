4b51eb70cf57fa3c99f5bc4fbaf647b6750ac5c4
Robert Petrella <36828415+RobertPetrella@users.noreply.github.com>
2026-08-17 16:25:19 +1000

    add basic azure vm config

--- main.tf
+++ main.tf
@@ -47,3 +47,38 @@ resource "azurerm_network_interface" "nic" {
     private_ip_address_allocation = "Dynamic"
   }
 }
+
+#Configure Azure Ubuntu VMs
+resource "azurerm_virtual_machine" "vm" {
+  name = "$azurevm-vm"
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  vm_size = "Standard_DS1_v2"
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer = "0001-com-ubuntu-server-jammy"
+    sku = "22-04-lts"
+    version = "latest"
+  }
+
+  storage_os_disk {
+    name = "mainosdisk1"
+    caching = ="ReadWrite"
+    create_option = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name = "examplewebserver"
+    admin_username = "fakeadmin"
+    admin_password = "FakePassword1234!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "staging"
+  }
+}
